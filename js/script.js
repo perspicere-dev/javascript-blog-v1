fcb9e850a33ff00332bc0000819973cd143071be
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-04-20 18:22:05 +0200

    finish genarateTags function

--- js/script.js
+++ js/script.js
@@ -42,10 +42,11 @@ const titleClickHandler = function(event) { //wyświetlanie artykułu po klikni�
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(){ // generowanie listy tytułow
-
+  
   /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
@@ -84,3 +85,50 @@ function generateTitleLinks(){ // generowanie listy tytułow
 }
 
 generateTitleLinks();
+
+function generateTags(){
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log('articles', articles);
+
+  /* START LOOP: for every article: */
+  for (let article of articles){
+
+    /* find tags wrapper */
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    console.log('tagsWrapper', tagsWrapper);
+
+    /* make html variable with empty string */
+    let html = ' ';
+
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    console.log('articleTags', articleTags);
+
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    console.log('articleTagsArray', articleTagsArray)
+
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray){
+    console.log('tag', tag);
+
+      /* generate HTML of the link */
+      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+      console.log('HTML of tag: ', linkHTML);
+
+      /* add generated code to html variable */
+      html = html + linkHTML;
+
+    /* END LOOP: for each tag */
+    }
+
+    /* insert HTML of all the links into the tags wrapper */
+    tagsWrapper.innerHTML = html;
+    console.log('tagsWrapper HTML: ', html)
+
+  /* END LOOP: for every article: */
+  }
+}
+
+generateTags();

7beaad5834a80a186c232ee39ff3d18cc896273d
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-04-21 11:14:48 +0200

    chabge console log for href in tagClickHandler

--- js/script.js
+++ js/script.js
@@ -143,7 +143,7 @@ function tagClickHandler(event){
 
   /* make a new constant "href" and read the attribute "href" of the clicked element */
   const href = clickedElement.getAttribute('href');
-  console.log('href', href);
+  console.log('href of clicked tag', href);
 
   /* make a new constant "tag" and extract tag from the "href" constant */
   const tag = href.replace('#tag-', '');
